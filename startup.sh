cd647e6948ba6ade4337f528a9ed6eb67dd16462
Gabriel <gpopesc@gmail.com>
2021-08-09 23:45:57 +0300

    initial

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-exec >> tmp/mylogfile.log 2>&1
-
-echo 'deb http://download.opensuse.org/repositories/home:/Alexx2000/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/home:Alexx2000.list
-curl -fsSL https://download.opensuse.org/repositories/home:Alexx2000/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_Alexx2000.gpg > /dev/null
+exec >> /tmp/startup.log 2>&1
 
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
@@ -16,35 +13,44 @@ if [ -e "/usr/share/applications/google-chrome.desktop" ]
 fi
 
 mkdir /tmp/.ICE-unix && chmod 1777 /tmp/.ICE-unix
-echo "root pass"
 echo root:${VNCPASS} | sudo chpasswd
-#chmod 777 /usr/share/plank/themes/Default/dock.theme
+cp /tmp/config/index.html /opt/noVNC/index.html
 
 if [ -n "${USER_NAME}" ]
  then
-  echo "Running as user ${USER_NAME}"
+  echo "Running as user ${USER_NAME} with id ${UID} and groupid ${GID}"
   #set default root password
-  useradd -m -p $(openssl passwd -1 ${USER_PASSWORD}) -s /bin/bash -G sudo ${USER_NAME}
-  sudo usermod -a -G root ${USER_NAME}
+  groupadd -f -g ${GID} users
+  groupmod -g ${GID} users
+  useradd -u ${UID} -g ${GID} -m -p $(openssl passwd -1 ${USER_PASSWORD}) -s /bin/bash -G sudo ${USER_NAME}
+  find /home/${USER_NAME} -path /home/${USER_NAME}/share -prune -o -exec chown ${UID}:${GID} {} \;
+  usermod -a -G root ${USER_NAME} && usermod -a -G audio ${USER_NAME} && usermod -a -G users ${USER_NAME}
   export HOME=/home/${USER_NAME}
+  echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S mkdir -p /home/${USER_NAME}/share
   echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S mkdir -p /home/${USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml
-  echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S cp /tmp/*.xml /home/${USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml/
-  cp /root/capslock_toggle.sh /home/${USER_NAME}/capslock_toggle.sh && chmod 777 /home/${USER_NAME}/capslock_toggle.sh
-  #echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S chown ${USER_NAME}:0 /home/${USER_NAME}/capslock_toggle.sh
+  echo ${USER_PASSWORD} | sudo -u ${USER_NAME} -S cp -nv /tmp/config/*.xml /home/${USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml/
+  cp /tmp/config/capslock_toggle.sh /home/${USER_NAME}/capslock_toggle.sh && chmod 777 /home/${USER_NAME}/capslock_toggle.sh
+  sed -i "s|unix:/run/user/U_ID/pulse/native|unix:/run/user/$UID/pulse/native|" /tmp/config/pulse-client.conf && \
+  cp -v /tmp/config/pulse-client.conf /etc/pulse/client.conf && \
   echo "cd /home/${USER_NAME}" >> ~/.bashrc
+  if [ -e "~/.bashrc" ]
+    then
+  echo ".bashrc exist --> skipping"
+    else
+  echo "cd /home/${USER_NAME}" >> ~/.bashrc
+  fi
+  echo "===========> script finnished <============" && \
   sudo -u ${USER_NAME} startxfce4 & \
-  #killall plank && sleep 10 && sudo -u ${USER_NAME} plank &
-  #echo "===========> before sleep <============" && sleep 60 && \
   #echo ${USER_PASSWORD} | sudo -u ${USER_NAME} xfconf-query --channel thunar --property /misc-exec-shell-scripts-by-default --create --type bool --set true && \
-  #echo "===========> script finnished <============"
  else
   echo "Running as root"
+  mkdir -p /root/share
   mkdir -p /root/.config/xfce4/xfconf/xfce-perchannel-xml
-  cp /tmp/*.xml /root/.config/xfce4/xfconf/xfce-perchannel-xml/
-  nohup startxfce4 &>/dev/null & \
-  #killall plank && sleep 10 && plank &
-  #allow bash script running from thunar
-  #echo "===========> before sleep <============" && sleep 60 && \
+  cp -nv /tmp/config/*.xml /root/.config/xfce4/xfconf/xfce-perchannel-xml/
+  cp /tmp/config/capslock_toggle.sh /home/${USER_NAME}/capslock_toggle.sh && chmod 700 /home/${USER_NAME}/capslock_toggle.sh 
+  echo "===========> script finnished <============" && \
+  startxfce4 & \
   #xfconf-query --channel thunar --property /misc-exec-shell-scripts-by-default --create --type bool --set true && \
-  #echo "===========> script finnished <============"
 fi
+rm -rf /tmp/config
+echo "===========> Done <============"
